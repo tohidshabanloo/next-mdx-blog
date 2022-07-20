75f2f9abe71c40e98a3985086972a10ca1ea27c9
tohidshabanloo <tshabanloo@hotmail.com>
2022-07-20 21:53:17 +0430

    new

--- components/Post.js
+++ components/Post.js
@@ -6,7 +6,7 @@ export default function Post({ post }) {
       <div className="card">
         <img className="img" src={post.frontmatter.cover_image} alt="" />
         <div className="post-date">
-          <h3>"{post.frontmatter.title}"</h3>
+          <h3>{post.frontmatter.title}</h3>
           <h4>اثر : {post.frontmatter.athour} </h4>
           <h4>مترجم :{post.frontmatter.translator} </h4>
         </div>
