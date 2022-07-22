26d42e18f28c78f834b0070a0c95bd46fa552021
tohidshabanloo <tshabanloo@hotmail.com>
2022-07-22 12:28:27 +0430

    made nav

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { navLinks } from "./navLinks";
 
 export default function Header() {
   return (
@@ -8,6 +9,17 @@ export default function Header() {
         <Link href="/" passHref>
           <h2>پادکستر</h2>
         </Link>
+          <nav className="nav">
+            {navLinks.map((link, index) => {
+              return (
+                <div className="every-nav" key={index}>
+                  <Link href={link.path}>
+                    <div key={index}>{link.name}</div>
+                  </Link>
+                </div>
+              );
+            })}
+          </nav>
       </div>
     </header>
   );
---
+++ components/navLinks.js
@@ -0,0 +1,19 @@
+export const navLinks = [
+  { name: "خانه", path: "/" },
+  {
+    name: "درباره ما",
+    path: "/about",
+  },
+  {
+    name: "خدمات",
+    path: "/services",
+  },
+  {
+    name: "وبلاگ",
+    path: "/blog",
+  },
+  {
+    name: "تماس با ما",
+    path: "#contact",
+  },
+];
