5a59c787797883e51efd6797cf2a23a6f8f61005
tohidshabanloo <tshabanloo@hotmail.com>
2022-07-26 09:58:37 +0430

    css changed

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <footer>
       <div className="footerText">
+        <h4> بازنشر فقط با ذكر لينك مجاز است.</h4>
         <Link target="_blank" href="https://tohidsh.com" passHref>
           <h4 className="text1"> توحید شعبانلو</h4>
         </Link>
-        <h4> تیر 1401</h4>
       </div>
     </footer>
   );
